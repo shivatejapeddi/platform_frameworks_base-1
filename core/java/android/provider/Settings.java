8b967966f1f6965f7c1085334b2195fce9746890
LorDClockaN <lordclockan@gmail.com>
2017-09-19 19:27:58 +0800

    Powermenu Torch option [1/2]
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Ib65e075bbbc1e3f2ff8d98ab0770f827b9c397bc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4897,6 +4897,13 @@ public final class Settings {
         public static final String POWERMENU_LOGOUT = "powermenu_logout";
 
         /**
+         * Whether to display the torch option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_TORCH = "powermenu_torch";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
