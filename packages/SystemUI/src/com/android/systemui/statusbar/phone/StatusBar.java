708bf69dc3b9c071d03d2ad8e6551b03d8c5d271
Decad3nce <decad3nce@gmail.com>
2017-09-28 21:16:26 +0800

    SystemUI: HeadsUp blacklists (1/2)
    Change-Id: Ia9b4e5df113f1c92f25c78ea4a804f73933c8990
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -482,6 +482,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     ActivityManager mAm;
     private ArrayList<String> mStoplist = new ArrayList<String>();
+    private ArrayList<String> mBlacklist = new ArrayList<String>();
 
     // the tracker view
     int mTrackingPosition; // the position of the top of the tracking view.
@@ -5567,6 +5568,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.HEADS_UP_STOPLIST_VALUES), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_BLACKLIST_VALUES), false, this);
             update();
         }
 
@@ -5620,6 +5623,11 @@ public class StatusBar extends SystemUI implements DemoMode,
                 final String stopString = Settings.System.getString(mContext.getContentResolver(),
                         Settings.System.HEADS_UP_STOPLIST_VALUES);
                 splitAndAddToArrayList(mStoplist, stopString, "\\|");
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.HEADS_UP_BLACKLIST_VALUES))) {
+                final String blackString = Settings.System.getString(mContext.getContentResolver(),
+                        Settings.System.HEADS_UP_BLACKLIST_VALUES);
+                splitAndAddToArrayList(mBlacklist, blackString, "\\|");
             }
         }
 
@@ -5633,6 +5641,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             setLockscreenMediaMetadata();
             setQsRowsColumns();
             setHeadsUpStoplist();
+            setHeadsUpBlacklist();
         }
     }
 
@@ -5674,6 +5683,12 @@ public class StatusBar extends SystemUI implements DemoMode,
         final String stopString = Settings.System.getString(mContext.getContentResolver(),
                     Settings.System.HEADS_UP_STOPLIST_VALUES);
         splitAndAddToArrayList(mStoplist, stopString, "\\|");
+    }
+
+    private void setHeadsUpBlacklist() {
+        final String blackString = Settings.System.getString(mContext.getContentResolver(),
+                    Settings.System.HEADS_UP_BLACKLIST_VALUES);
+        splitAndAddToArrayList(mBlacklist, blackString, "\\|");
     }
 
     private RemoteViews.OnClickHandler mOnClickHandler = new RemoteViews.OnClickHandler() {
@@ -7247,8 +7262,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         List<ActivityManager.RunningTaskInfo> taskInfo = mAm.getRunningTasks(1);
         ComponentName componentInfo = taskInfo.get(0).topActivity;
 
-        if(isPackageInStoplist(componentInfo.getPackageName())
-                && !isDialerApp(sbn.getPackageName())) {
+        if(isPackageBlacklisted(sbn.getPackageName())
+                || (isPackageInStoplist(componentInfo.getPackageName())
+                && !isDialerApp(sbn.getPackageName()))) {
             return false;
         }
 
@@ -7316,6 +7332,10 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private boolean isPackageInStoplist(String packageName) {
         return mStoplist.contains(packageName);
+    }
+
+    private boolean isPackageBlacklisted(String packageName) {
+        return mBlacklist.contains(packageName);
     }
 
     private boolean isDialerApp(String packageName) {
