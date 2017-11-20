AOSPA/android_frameworks_base-old

SystemUI: Support recents task locking
This adds the ability to lock task in recents so they won't be cleared when removing all tasks

Ticket: NOUGAT-1

Drawables from material.io

Change-Id: I06ffe411e72d4e837cfec7b9b77fb2519e417bc7

packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -57,6 +57,7 @@ import com.android.systemui.recents.events.component.ShowUserToastEvent;
 import com.android.systemui.recents.events.ui.RecentsDrawnEvent;
 import com.android.systemui.recents.misc.SystemServicesProxy;
 import com.android.systemui.recents.model.RecentsTaskLoader;
+import com.android.systemui.recents.model.Task;
 import com.android.systemui.recents.tv.RecentsTvImpl;
 import com.android.systemui.stackdivider.Divider;
 
@@ -84,6 +85,8 @@ public class Recents extends SystemUI
         RECENTS_ACTIVITIES.add(RecentsImpl.RECENTS_ACTIVITY);
         RECENTS_ACTIVITIES.add(RecentsTvImpl.RECENTS_TV_ACTIVITY);
     }
+
+    public final static ArrayList<Task> sLockedTasks = new ArrayList<>();
 
     // Purely for experimentation
     private final static String RECENTS_OVERRIDE_SYSPROP_KEY = "persist.recents_override_pkg";

shivatejapeddi/platform_frameworks_base-1

  This adds the ability to lock task in recents so they won't be cleared when removing all tasks
  Ticket: NOUGAT-1
  Drawables from material.io
  Change-Id: I06ffe411e72d4e837cfec7b9b77fb2519e417bc7
  SystemUI: animate icon when locking a task
  Change-Id: Ic153cb3707a14dba96d8889b11f149a339fb55e0
  Signed-off-by: Joey Rizzoli <joey@lineageos.org>
  SystemUI: allow user to dismiss a locked task by swiping it away manually
  Change-Id: Icf4e851a9d9dead5ea6c25dd2e020b814af0b779
Change-Id: I882f741ba518090202203e4326a5cf7205ebc098
Signed-off-by: mydongistiny <jaysonedson@gmail.com>
Signed-off-by: xyyx <xyyx@mail.ru>

packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -58,6 +58,7 @@ import com.android.systemui.recents.events.component.ShowUserToastEvent;
 import com.android.systemui.recents.events.ui.RecentsDrawnEvent;
 import com.android.systemui.recents.misc.SystemServicesProxy;
 import com.android.systemui.recents.model.RecentsTaskLoader;
+import com.android.systemui.recents.model.Task;
 import com.android.systemui.stackdivider.Divider;
 import com.android.systemui.statusbar.CommandQueue;
 
@@ -86,6 +87,8 @@ public class Recents extends SystemUI
     static {
         RECENTS_ACTIVITIES.add(RecentsImpl.RECENTS_ACTIVITY);
     }
+
+    public final static Set<Task> sLockedTasks = new HashSet<>();
 
     // Purely for experimentation
     private final static String RECENTS_OVERRIDE_SYSPROP_KEY = "persist.recents_override_pkg";